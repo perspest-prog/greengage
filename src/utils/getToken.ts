eba7a31b21403f7bd0378a57894065ff485a30da
Ivan Avilov <ivan.avilov.0606@mail.ru>
2025-05-13 23:44:26 +0300

    token...

--- src/utils/getToken.ts
+++ src/utils/getToken.ts
@@ -2,7 +2,7 @@ import { env } from "node:process"
 import { readFileSync } from "node:fs"
 
 const getToken = () => {
-    return env.NODE_ENV === "production" ? readFileSync("/run/secrets/TOKEN", { encoding: "utf-8" }) : env.TOKEN
+    return env.NODE_ENV === "production" ? readFileSync("/run/secrets/token", { encoding: "utf-8" }) : env.TOKEN
 }
 
 export default getToken
